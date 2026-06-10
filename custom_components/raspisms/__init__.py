8035944f92880565b4fb2a682923b1500f80f10e
Stéphane BARRE <stephane.barre.35760@gmail.com>
2026-06-10 21:40:42 +0200

    1.0.8

--- custom_components/raspisms/__init__.py
+++ custom_components/raspisms/__init__.py
@@ -258,8 +258,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
                                 if entitiy_with_label.domain == "camera"
                             ]
 
-                            for _number in numbers:
-                                for _camera in cameras:
+                            for _camera in cameras:
+                                for _number in numbers:
                                     _LOGGER.debug(f"Bell '{ message } avec le label '{ label }' : '{ _camera }' sur { _number }")
                                     await snapshot_camera( _camera)
                                     await alert_camera( _number, message, _camera, cmd="BELL")
@@ -297,8 +297,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
                                 if entitiy_with_label.domain == "camera"
                             ]
 
-                            for _number in numbers:
-                                for _camera in cameras:
+                            for _camera in cameras:
+                                for _number in numbers:
                                     _LOGGER.debug(f"Alerte '{ message } avec le label '{ label }' : '{ _camera }' sur { _number }")
                                     await alert_camera( _number, message, _camera, cmd="ALERT")
                                 
